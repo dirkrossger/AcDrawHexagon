7cda948972367728219abe4d5e6a2a0f8f997173
dirkrossger <dirk.roessger@gmail.com>
2019-04-24 10:19:17 +0200

    DrawHexagon

---
+++ DrawHexagon/Commands.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DrawHexagon;
+
+#region Autodesk libraries
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+#endregion
+
+[assembly: CommandClass(typeof(DrawHexagon.Commands))]
+
+namespace DrawHexagon
+{
+    class Commands
+    {
+        [CommandMethod("xHexa")]
+        public void Start()
+        {
+            Database db = Active.Database;
+            Hexagon coord = new Hexagon();
+            List<PointF[]> arr = coord.DrawHexGrid(0, 100, 0, 100, 5);
+            Point3dCollection p3 = new Point3dCollection();
+
+            try
+            {
+                foreach (PointF[] hexa in arr)
+                {
+                    foreach (PointF pt in hexa)
+                    {
+                        p3.Add(new Point3d(pt.X, pt.Y, 0));
+                    }
+                }
+                
+                
+
+
+                using (Transaction acTrans = db.TransactionManager.StartTransaction())
+                {
+                    // Open the Block table for read
+                    BlockTable acBlkTbl;
+                    acBlkTbl = acTrans.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+
+                    // Open the Block table record Model space for write
+                    BlockTableRecord acBlkTblRec;
+                    acBlkTblRec = acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+
+                    if (p3 != null)
+                    {
+                        foreach (Point3d pt in p3)
+                        {
+                            using (DBPoint acPoint = new DBPoint(pt))
+                            {
+                                // Add the new object to the block table record and the transaction
+                                acBlkTblRec.AppendEntity(acPoint);
+                                acTrans.AddNewlyCreatedDBObject(acPoint, true);
+                            }
+                        }
+                    }
+                    // Set the style for all point objects in the drawing
+                    db.Pdmode = 34;
+                    db.Pdsize = 0.1;
+
+                    // Save the new object to the database
+                    acTrans.Commit();
+                }
+            }
+            catch(System.Exception ex) { }
+
+            
+        }
+
+    }
+
+    public static class Active
+    {
+        /// <summary>
+        /// Returns the active Editor object.
+        /// </summary>
+        public static Editor Editor
+        {
+            get { return Document.Editor; }
+        }
+
+        /// <summary>
+        /// Returns the active Document object.
+        /// </summary>
+        public static Document Document
+        {
+            get { return Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument; }
+        }
+
+        /// <summary>
+        /// Returns the active Database object.
+        /// </summary>
+        public static Database Database
+        {
+            get { return Document.Database; }
+        }
+
+        /// <summary>
+        /// Sends a string to the command line in the active Editor
+        /// </summary>
+        /// <param name="message">The message to send.</param>
+        public static void WriteMessage(string message)
+        {
+            Editor.WriteMessage(message);
+        }
+
+        /// <summary>
+        /// Sends a string to the command line in the active Editor using String.Format.
+        /// </summary>
+        /// <param name="message">The message containing format specifications.</param>
+        /// <param name="parameter">The variables to substitute into the format string.</param>
+        public static void WriteMessage(string message, params object[] parameter)
+        {
+            Editor.WriteMessage(message, parameter);
+        }
+    }
+
+    public class Extension : IExtensionApplication
+    {
+        [CommandMethod("info")]
+        public void Initialize()
+        {
+            Active.Editor.WriteMessage("\n-> Get Points from Hexagon: xHexa");
+        }
+
+        public void Terminate()
+        {
+        }
+    }
+
+}
---
+++ DrawHexagon/Hexagon.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawHexagon
+{
+
+    /// <summary>
+    /// http://csharphelper.com/blog/2015/10/draw-a-hexagonal-grid-in-c/
+    /// </summary>
+    class Hexagon
+    {
+        // Return the points that define the indicated hexagon.
+        private PointF[] HexToPoints(float height, float row, float col)
+        {
+            // Start with the leftmost corner of the upper left hexagon.
+            float width = HexWidth(height);
+            float y = height / 2;
+            float x = 0;
+
+            // Move down the required number of rows.
+            y += row * height;
+
+            // If the column is odd, move down half a hex more.
+            if (col % 2 == 1) y += height / 2;
+
+            // Move over for the column number.
+            x += col * (width * 0.75f);
+
+
+            // Generate the points.
+            return new PointF[]
+                {
+                    new PointF(x, y),
+                    new PointF(x + width * 0.25f, y - height / 2),
+                    new PointF(x + width * 0.75f, y - height / 2),
+                    new PointF(x + width, y),
+                    new PointF(x + width * 0.75f, y + height / 2),
+                    new PointF(x + width * 0.25f, y + height / 2),
+                };
+        }
+
+        // Return the width of a hexagon.
+        private float HexWidth(float height)
+        {
+            return (float)(4 * (height / 2 / Math.Sqrt(3)));
+        }
+
+        // Draw a hexagonal grid for the indicated area.
+        // (You might be able to draw the hexagons without
+        // drawing any duplicate edges, but this is a lot easier.)
+        public List<PointF[]> DrawHexGrid(float xmin, float xmax, float ymin, float ymax, float height)
+        {
+            List<PointF[]> result = new List<PointF[]>();
+           
+            // Loop until a hexagon won't fit.
+            for (int row = 0;  ; row++)
+            {
+                //// Get the points for the row's first hexagon.
+                PointF[] points = HexToPoints(height, row, 0);
+
+                //// If it doesn't fit, we're done.
+                if (points[4].Y > ymax)
+                    break;
+                else
+                {
+                    // Draw the row.
+                    for (int col = 0; ; col++)
+                    {
+                        // Get the points for the row's next hexagon.
+                        points = HexToPoints(height, row, col);
+
+                        // If it doesn't fit horizontally,
+                        // we're done with this row.
+                        if (points[3].X > xmax) break;
+
+                        result.Add(points);
+                    }
+                }
+            }
+            return RemoveDuplicates(result);
+        }
+
+        private List<PointF[]> RemoveDuplicates(List<PointF[]> points)
+        {
+            for (int i = 0; i < points.Count; i++)
+            {
+                int index = 0;
+
+                while (index < points.Count - 1)
+                {
+                    if (points[i].SequenceEqual(points[index + 1]))
+                        points.RemoveAt(i);
+                    else
+                        index++;
+                }
+            }
+
+            return points;
+        }
+    }
+}
+
+internal class PointF
+{
+    public float X;
+    public float Y;
+
+    public PointF(float x, float y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+}
